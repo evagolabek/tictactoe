36732d0a1734732289d235f8fc9ad24c56fb3e01
evagolabek <golabek.eva@gmail.com>
2018-03-15 00:42:14 +0100

    tic tac toe start

---
+++ ttt.js
@@ -0,0 +1,16 @@
+let canvas = document.getElementById('ttt'), ctx = canvas.getContent('2d'), msg = document.getElementById('message');
+
+canvas.width = canvas.height 300;
+
+
+function draw () {
+
+}
+
+   function drawBoard () {
+
+   }
+
+   function fillBoard () {
+
+   }
